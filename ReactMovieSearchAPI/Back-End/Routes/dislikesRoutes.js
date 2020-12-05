5c29040f84947d94f92e6e95215c155577b7df96
Nilber Remon <nilberremon@gmail.com>
2020-12-04 23:37:13 -0500

    Likes and Dislikes Params Routes Fixes

--- ReactMovieSearchAPI/Back-End/Routes/dislikesRoutes.js
+++ ReactMovieSearchAPI/Back-End/Routes/dislikesRoutes.js
@@ -3,7 +3,7 @@ const dislikes = require("express").Router()
 const { getAllDislikes, getDislike, createDislike, deleteDislike }  = require("../Queries/dislikesQueries")
 
 //dislikes Routes
-dislikes.delete("/:userId/:filmId", deleteDislike)
+dislikes.delete("/:user_id/:film_id", deleteDislike)
 dislikes.get("/:id", getDislike)
 dislikes.get("/", getAllDislikes)
 dislikes.post("/", createDislike)
--- ReactMovieSearchAPI/Back-End/Routes/likesRoutes.js
+++ ReactMovieSearchAPI/Back-End/Routes/likesRoutes.js
@@ -3,7 +3,7 @@ const likes = require("express").Router()
 const { getAllLikes, getLike, createLike, deleteLike }  = require("../Queries/likesQueries")
 
 //likes Routes
-likes.delete("/:user/:filmId", deleteLike)
+likes.delete("/:user_id/:film_id", deleteLike)
 likes.get("/:id", getLike)
 likes.get("/", getAllLikes)
 likes.post("/", createLike)
